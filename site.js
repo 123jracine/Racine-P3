d45699109776841a64dc46aa8d5d6e57d9f1903a
123jracine <71267999+123jracine@users.noreply.github.com>
2023-03-01 10:15:20 -0500

    uploaded files

--- site.js
+++ site.js
@@ -33,13 +33,39 @@ const vue_app = Vue.createApp({
         return {
             // This holds your movies.json data.
             movies: [],
+            title: "IMDB + Jose's Top 8 Movies",
+            owner: "Jose",
+            github: "https://github.com/123jracine/Racine-P3",
+           
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+      
       }
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
+            getMonthTest(dataArray){
+
+            },
+            posterClick(index){
+                  let x = movies[index].posterIndex;
+                  let y = movies[index].posters.Length;
+
+                  if(x < y) {
+                        movies[index].posterIndex++;
+                  }
+                  else{
+                        movies[index].posterIndex = 0
+                  }
+            },
+            timeText(minutes){
+                  Math.trunc(x);
+            }
+      },
+
+
+
 })
+
+
 
 vue_app.mount("#vue_app")
